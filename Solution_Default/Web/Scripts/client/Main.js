2ba30061a48d1c6b1a5e0d993a0ccdb839f91059
HIEU <hieu.n2395@gmail.com>
2018-08-25 11:43:28 +0700

    Source 25-8-2018

--- Solution_Default/Web/Scripts/client/Main.js
+++ Solution_Default/Web/Scripts/client/Main.js
@@ -12,40 +12,41 @@
             $(".navbar.navbar-transparent").removeClass("menuSroll");
         }
 
-        //Posion 320 Information
-        if (positionCurrent >= 320)
-            $(".Information").addClass("effectLeft");
+        ////Posion 320 Information
+        //if (positionCurrent >= 320)
+        //    $(".Information").addClass("effectLeft");
 
-        //Posion 1040 New Product
-        if (positionCurrent >= 1040)
-        {
-            $(".newProductEffectLeft").addClass("effectRight");
-            $(".newProductEffectRight").addClass("effectBottom");
-        }
+        ////Posion 1040 New Product
+        //if (positionCurrent >= 1040)
+        //{
+        //    $(".newProductEffectLeft").addClass("effectLeft");
+        //    $(".newProductEffectTop").addClass("effectBottom");
+        //    $(".newProductEffectRight").addClass("effectRight");
+        //}
 
-        //Posion 2320 Top Product
-        if (positionCurrent >= 2320) {
-            $(".topProEffectLeft").addClass("effectLeft");
-            $(".topProEffectRight").addClass("effectBottom");
-        }
+        ////Posion 2320 Top Product
+        //if (positionCurrent >= 1900) {
+        //    $(".topProEffectLeft").addClass("effectLeft");
+        //    $(".topProEffectRight").addClass("effectBottom");
+        //}
 
-        //Posion 3760 Sale Product
-        if (positionCurrent >= 3040)
-        {
-            $(".saleProductEffectLeft").addClass("effectRight");
-            $(".saleProductEffectRight").addClass("effectBottom");
-        }
+        ////Posion 3760 Sale Product
+        //if (positionCurrent >= 2400)
+        //{
+        //    $(".saleProductEffectLeft").addClass("effectRight");
+        //    $(".saleProductEffectRight").addClass("effectBottom");
+        //}
 
-        //Position 4400 News
-        if (positionCurrent >= 4400)
-        {
-            $(".NewsProduct").addClass("effectBottom");
-        }
+        ////Position 4400 News
+        //if (positionCurrent >= 4000)
+        //{
+        //    $(".NewsProduct").addClass("effectBottom");
+        //}
 
-        if (positionCurrent >= 4960)
-        {
-            $(".Contact").addClass("effectLeft");
-        }
+        //if (positionCurrent >= 4940)
+        //{
+        //    $(".Contact").addClass("effectLeft");
+        //}
     });
 });
 
